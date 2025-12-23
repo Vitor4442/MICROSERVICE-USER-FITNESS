c40f0922a9b58b7d11462cbb8f76005763a8ad47
José Vitor <jose.vitorw018@gmail.com>
2025-12-22 21:40:54 -0300

    Feito o model user e a conexão com bd

---
+++ userserci/src/main/java/com/fitness/userserci/model/User.java
@@ -0,0 +1,35 @@
+package com.fitness.userserci.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String firsName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private  UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ userserci/src/main/java/com/fitness/userserci/model/UserRole.java
@@ -0,0 +1,5 @@
+package com.fitness.userserci.model;
+
+public enum UserRole {
+    USER, ADMIN
+}
